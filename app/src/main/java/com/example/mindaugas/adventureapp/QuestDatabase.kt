0535b60b01b710ea6331d3f6d35c2e8c194100b7
NeuTronas <nequadora@gmail.com>
2018-07-05 14:55:00 +0300

    New location

--- app/src/main/java/com/example/mindaugas/adventureapp/QuestDatabase.kt
+++ app/src/main/java/com/example/mindaugas/adventureapp/QuestDatabase.kt
@@ -8,6 +8,7 @@ class QuestDatabase{
 
     private fun generateMockQuests(): Map<String, Quest> {
         return mapOf("123456" to Quest("Sidnejus", "uzduotis australijoje", "02", LatLng(-34.0, 151.0)),
+                "123457" to Quest("Lukas programuoja", "Blemba kokia lengva uzduotis", "02", LatLng(54.926746, 24.100789)),
                 "123555" to Quest("Radvilenai", "uzduotis namuose","03", LatLng(54.90720214, 23.93623281)))
     }
 }
