59d0533f3f7aaba202876f452957e86bb0c47e8f
Demings <mindaugas.laskis@gmail.com>
2018-07-05 17:08:16 +0300

    added a proxmity check using Haversine formula

--- app/src/main/java/com/example/mindaugas/adventureapp/MapsActivity.kt
+++ app/src/main/java/com/example/mindaugas/adventureapp/MapsActivity.kt
@@ -158,12 +158,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
                 requestLocationPermission()
             }else{
                 if(currentLocation != null){
-                    showQuestDialog(it.tag as Quest)
+                    if(getDistanceFromLatLonInKm(
+                                    LatLng(currentLocation!!.latitude, currentLocation!!.longitude),
+                                    it.position) < Constants.GEOFENCE_RADIUS_IN_METERS / 1000){
+                        showQuestDialog(it.tag as Quest)
+                    }else{
+                        Toast.makeText(this, "Too far!", Toast.LENGTH_SHORT).show()
+                    }
                 }else{
                     //location is null
                 }
             }
         }
+    }
+
+
+    fun getDistanceFromLatLonInKm(location1: LatLng, location2: LatLng): Double {
+        var R = 6371 // Radius of the earth in km
+        var dLat = deg2rad(location2.latitude - location1.latitude)  // deg2rad below
+        var dLon = deg2rad(location2.longitude - location1.longitude)
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                        Math.cos(deg2rad(location1.latitude)) * Math.cos(deg2rad(location2.latitude)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2)
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        var d = R * c // Distance in km
+        return d
+    }
+
+    fun deg2rad(deg: Double): Double {
+        return deg * (Math.PI/180)
     }
 
     fun showQuestDialog(quest: Quest) {
