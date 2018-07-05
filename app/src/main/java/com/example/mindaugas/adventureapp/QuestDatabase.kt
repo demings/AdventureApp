e7ddb547048a04a6839207b74e0ae261ac0549df
DESKTOP-3A4VCUD\Worll <kvedarasarnas@gmail.com>
2018-07-05 15:22:16 +0300

    New locations

--- app/src/main/java/com/example/mindaugas/adventureapp/QuestDatabase.kt
+++ app/src/main/java/com/example/mindaugas/adventureapp/QuestDatabase.kt
@@ -8,7 +8,8 @@ class QuestDatabase{
 
     private fun generateMockQuests(): Map<String, Quest> {
         return mapOf("123456" to Quest("Sidnejus", "uzduotis australijoje", "02", LatLng(-34.0, 151.0)),
-                "123457" to Quest("Lukas programuoja", "Blemba kokia lengva uzduotis", "02", LatLng(54.926746, 24.100789)),
+                "123457" to Quest("Lekt. Magistr. Doc. Habil. Dakt. Litenantas Lukas Alfredas Ružauskas", "Blemba kokia lengva uzduotis", "02", LatLng(54.926746, 24.100789)),
+                "123458" to Quest("Arnelio Kvedaraelio", "Imink misle", "02", LatLng(55.4095649, 23.7285911)),
                 "123555" to Quest("Radvilenai", "uzduotis namuose","03", LatLng(54.90720214, 23.93623281)))
     }
 }
