7d9efd43b92b3cd785919cdce7af7111e5ddd376
Demings <mindaugas.laskis@gmail.com>
2018-07-07 16:45:35 +0300

    refactoring

--- app/src/main/java/com/example/mindaugas/adventureapp/MapsActivity.kt
+++ app/src/main/java/com/example/mindaugas/adventureapp/MapsActivity.kt
@@ -2,37 +2,35 @@ package com.example.mindaugas.adventureapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.CameraPosition
 import android.location.Criteria
 import android.location.Location
 import android.location.LocationManager
-import android.util.Log
-import com.google.android.gms.maps.model.MarkerOptions
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.Marker
-import android.app.PendingIntent;
-import android.content.ContentValues
-import android.content.Intent
-import android.nfc.Tag
 import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
 
@@ -47,6 +45,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
     // geofencing
     lateinit var geofencingClient: GeofencingClient
     private var geofenceList : MutableList<Geofence> = mutableListOf()
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(this, GeofenceTransitionsIntentService::class.java)
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
 
 
     var firebase = Firebase()
@@ -249,13 +254,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
             setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
             addGeofences(geofenceList)
         }.build()
-    }
-
-    private val geofencePendingIntent: PendingIntent by lazy {
-        val intent = Intent(this, GeofenceTransitionsIntentService::class.java)
-        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
-        // addGeofences() and removeGeofences().
-        PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
     fun addGeofenceForQuest(id: String, quest: Quest){
